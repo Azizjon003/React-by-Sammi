6ffacee8cb5feff4f65371240672c33298340b2d
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2023-04-17 12:26:56 +0500

    Statelar bilan ishlashga tajriba

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,19 +1,52 @@
 import "./card.css";
-
-function Card({ name, views, favourite }) {
-  return (
-    <div
-      className={`d-flex justify-content-between align-items-center p-2 mt-2 
+import { Component } from "react";
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favourite: false,
+    };
+  }
+  onFavourite = () => {
+    this.setState((prevState) => ({
+      favourite: !prevState.favourite,
+    }));
+  };
+  render() {
+    const { name, views } = this.props;
+    const { favourite } = this.state;
+    return (
+      <div
+        className={`d-flex justify-content-between align-items-center p-2 mt-2 
     card-container ${favourite ? "favourite" : ""}`}
-    >
-      <p className="flex-grow-1 m-0">{name}</p>
-      <input type="number" defaultValue={views} className="input-card" />
-      <div className="d-flex align-items-center ">
-        <i className="fas fa-cookie-bite cookie-card"></i>
-        <i className="fas fa-trash trash-card "></i>
+      >
+        <p className="flex-grow-1 m-0">{name}</p>
+        <input type="number" defaultValue={views} className="input-card" />
+        <div className="d-flex align-items-center ">
+          <i
+            className="fas fa-cookie-bite cookie-card"
+            onClick={this.onFavourite}
+          ></i>
+          <i className="fas fa-trash trash-card "></i>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+// function Card({ name, views, favourite }) {
+//   return (
+//     <div
+//       className={`d-flex justify-content-between align-items-center p-2 mt-2
+//     card-container ${favourite ? "favourite" : ""}`}
+//     >
+//       <p className="flex-grow-1 m-0">{name}</p>
+//       <input type="number" defaultValue={views} className="input-card" />
+//       <div className="d-flex align-items-center ">
+//         <i className="fas fa-cookie-bite cookie-card"></i>
+//         <i className="fas fa-trash trash-card "></i>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Card;
